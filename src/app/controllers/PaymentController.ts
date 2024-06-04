e9d2037212d1465f753a8900c5a3758e18229cf7
MaiUoc <tinhmai775@gmail.com>
2024-06-04 09:49:27 +0700

    Final

--- src/app/controllers/PaymentController.ts
+++ src/app/controllers/PaymentController.ts
@@ -166,7 +166,7 @@ class PaymentController {
             var requestId = partnerCode + new Date().getTime();
             var orderId = requestId;
             var orderInfo = req.body.orderInfo;
-            var redirectUrl = "http://localhost:3000/payment/result";
+            var redirectUrl = "https://study365.vercel.app/payment/result";
             var ipnUrl = "https://eoidnudnipocu1q.m.pipedream.net/";
             var amount = req.body.amount;
             var requestType = "captureWallet"
